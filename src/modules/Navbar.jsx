a4b2e2f3c5ac2d39b268b46c0f56843fb1d8ee87
Jelenal1 <113020871+Jelenal1@users.noreply.github.com>
2023-03-13 16:22:27 +0100

    I parameterize

--- src/modules/Navbar.jsx
+++ src/modules/Navbar.jsx
@@ -1,13 +1,13 @@
 import { AiOutlineMenu, AiOutlinePlus } from "react-icons/ai";
 
-function Navabar() {
+function Navabar({ PageName }) {
   return (
     <>
       <div className="border-b-2 border-black h-16 p-2 flex justify-between items-center">
         <button>
           <AiOutlineMenu className="h-10 w-10" />
         </button>
-        <span className="text-3xl">Semester</span>
+        <span className="text-3xl">{PageName}</span>
         <button>
           <AiOutlinePlus className="w-12 h-12" />
         </button>
--- src/modules/SemesterList.jsx
+++ src/modules/SemesterList.jsx
@@ -3,7 +3,7 @@ import Navabar from "./Navbar";
 function SemesterList({ items }) {
   return (
     <>
-      <Navabar />
+      <Navabar PageName={"Semester"} />
       <div>
         <div>{`${items.length} Item`}</div>
       </div>
