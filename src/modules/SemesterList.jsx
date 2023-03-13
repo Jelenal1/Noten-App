2d216468b439084faf82a2e1f9c48df52e92cd40
Jelenal1 <113020871+Jelenal1@users.noreply.github.com>
2023-03-13 16:35:41 +0100

    Did some styling

--- src/modules/SemesterList.jsx
+++ src/modules/SemesterList.jsx
@@ -5,7 +5,9 @@ function SemesterList({ items }) {
     <>
       <Navabar PageName={"Semester"} />
       <div>
-        <div>{`${items.length} Item`}</div>
+        <div className="bg-blue-500 flex justify-end">
+          <span className="text-white text-lg font-bold mr-1">{`${items.length} Item`}</span>
+        </div>
       </div>
     </>
   );
